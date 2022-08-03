9082f802e5da47182b941502161dafce691e9ad6
Cole Ferguson <fergusoncole98@outlook.com>
2022-08-03 15:39:09 -0500

    Kanye and Ron Dialogue

---
+++ Kanye and Ron/Program.cs
@@ -0,0 +1,25 @@
+﻿using Kanye_and_Ron;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+
+for (int i = 0; i < 5; i++)
+{
+    QuoteGenerator.KanyeQuote();
+    QuoteGenerator.RonQuote();
+}
+
+//QuoteGenerator.KanyeQuote();
+//Console.WriteLine("Story Teller: Sir please I'm trying to....");
+//QuoteGenerator.RonQuote();
+//Console.WriteLine("Story Teller: Guys let me...");
+//QuoteGenerator.KanyeQuote();
+//Console.WriteLine("Story Teller: Jeez");
+//QuoteGenerator.RonQuote();
+//Console.WriteLine("Story Teller: Alright that's it, I guess I won't share my story");
+//QuoteGenerator.KanyeQuote();
+//QuoteGenerator.RonQuote();
+
+
+
